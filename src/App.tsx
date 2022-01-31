1847126a878cdfcc78b3894e1475549738c16646
Joan Wilson <joanwmoliveira@gmail.com>
2022-01-31 19:43:17 -0300

    estruturando projeto e baixando dependências

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Home from 'pages/Home';
-import Contact from 'pages/Contact';
-import Navbar from "components/Navbar";
+//import Home from 'pages/Home';
+//import Contact from 'pages/Contact';
+//import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    /*<BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contato" element={<Contact />}/>
-      </Routes>
-    </BrowserRouter>
+      <Routes>
+    </BrowserRouter>*/
+    <h1>Olá Mundo!</h1>
   );
 }
 
